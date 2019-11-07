f09dd9982e45300c3b80c868e9cb8d9cde590bbd
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-07 16:53:20 +0800

    Update call4ndre4s5.php

--- call4ndre4s5.php
+++ call4ndre4s5.php
@@ -17,7 +17,6 @@ function send($phone){
 echo "Lo Ngapain Di Script Gua Babi ? Jangan Tau Make Aja Babi Lu Tau Memek Gak ? Itu Kesukaan Gua Anjing ; SGBTEAM\n\n";
 echo "NomorMusuhAnjing\nLoTauInputGakBabi? : ";
 $nomor = trim(fgets(STDIN));
-$jumlah = trim (fgets(STDIN));
 $execute = send($nomor);
 print $execute;
 ?>
